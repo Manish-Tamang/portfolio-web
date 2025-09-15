61b83835741e0259b36af430ad79725a762931bf
Manish-Tamang <maneshtamang833@gmail.com>
2025-09-16 00:00:00 +0545

    Add about page

---
+++ app/about/page.tsx
@@ -0,0 +1,10 @@
+import LayoutWrapper from "@/components/layout-wrapper"
+import AboutHero from "@/components/about-hero"
+
+export default function AboutPage() {
+  return (
+    <LayoutWrapper>
+      <AboutHero />
+    </LayoutWrapper>
+  )
+}
